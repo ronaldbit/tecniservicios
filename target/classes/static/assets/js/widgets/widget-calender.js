6e479b5eafd425c24c9e0e0511ea9c099969186a
moxita124 <207654475+moxita124@users.noreply.github.com>
2025-10-24 23:58:39 -0500

    Agregue el gitignore

--- target/classes/static/assets/js/widgets/widget-calender.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', function () {
-  const datepicker_inline = new Datepicker(document.querySelector('#pc-datepicker-6'), {
-    buttonClass: 'btn'
-  });
-});
